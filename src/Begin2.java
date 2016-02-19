f137afe812994ff9a946d3fbcab17615281927d3
r3bers <mike@solovjov.net>
2016-02-19 21:54:38 +0300

    Первый коммит в обучаемых файлах...

--- src/Begin2.java
+++ src/Begin2.java
@@ -2,7 +2,8 @@ import java.math.BigDecimal;
 import java.util.Scanner;
 
 /**
- * Created by odmin on 13.02.16.
+ * Created by r3bers on 13.02.16.
+ * Тестовие задания. Начальный 2. Площадь Квадрата.
  */
 
 public class Begin2 {
@@ -10,13 +11,12 @@ public class Begin2 {
     public static void main(String argc[]) {
         System.out.print("Введите сторону квадрата: ");
         Scanner scanner = new Scanner(System.in);
-        boolean b = true;
-        double a = 0;
+        boolean b;
         BigDecimal bigD = new BigDecimal("0");
         do {
             String s = scanner.nextLine();
             try {
-                a = Double.parseDouble(s);
+                double a = Double.parseDouble(s);
                 if (a <= 0) {
                     System.out.print("Квадрат не может быть с отрицательными сторонами, попробуйте ещё: ");
                     b = false;
