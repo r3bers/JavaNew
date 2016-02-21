f58f051eebe850116440d71fac0eac2b4ef44ac1
r3bers <mike@solovjov.net>
2016-02-21 03:52:58 +0300

    Коррекция по простым числам и счисление времени работы.

--- src/Primes.java
+++ src/Primes.java
@@ -23,8 +23,7 @@ public class Primes {
             try {
                 a = Long.parseLong(s);
                 if (a <= 1) {
-                    System.out.print(zapros + " не может быть сньше 2-х : ");
-                    b = false;
+                    throw new NumberFormatException("Почему меньше двух.");
                 } else {
                     b = true;
                 }
@@ -40,7 +39,8 @@ public class Primes {
         System.out.print("Введите не перый элемент из множества натуральных чисел N : ");
         long n = inputNum("Число N");
         List<Long> pri = new ArrayList<>();
-        String toCons = "";
+        String toCons = "{ ";
+        long start = System.currentTimeMillis();
         for (long i = 2; i <= n; i++) {
             boolean hasDivider = false;
             for (Long aPri : pri) {
@@ -51,16 +51,16 @@ public class Primes {
             }
             if (!hasDivider) {
                 pri.add(i);
-                if ((toCons + ", " + i).length() > consoleWidth) {
+                if ((toCons + i + ", ").length() > consoleWidth) {
                     System.out.println(toCons);
-                    toCons = i+"";
+                    toCons = i + ", ";
                 } else {
-                    toCons = toCons + ", " + i;
+                    toCons = toCons + i + ", ";
                 }
-//                System.out.print(i+", ");
             }
         }
-
-        System.out.println(toCons);
+        start = System.currentTimeMillis()-start;
+        System.out.println(toCons.substring(0,toCons.length()-2)+" }");
+        System.out.println("Затрачено " + start + " мс.");
     }
 }
