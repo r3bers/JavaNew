9e5a54bb50b46e12c96fba953965b750a80eba81
r3bers <mike@solovjov.net>
2016-02-22 18:59:24 +0300

    Пузырёк с прерыванием или без, и выводы по большим числам.

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -5,25 +5,57 @@ import java.util.Random;
  * Created by r3bers on 21.02.16.
  * Задание от Geekbrains
  * Самостоятельно реализовать пузырьковую сортировку массива.
+ * <p>
+ * Рассмотренны два метода.
+ * Со сравнением о наличии пузырька в проходе и без.
  */
 public class BubbleSort {
     public static void main(String[] args) {
+        int k;
+        boolean hasBubble;
 
-        int z[] = new int[10];
-        Random rnd = new Random();
-        for (int i = 0; i < z.length; i++) {
-            z[i] = rnd.nextInt(100);
-        }
-        System.out.print(Arrays.toString(z) + " -> ");
-        for (int i = 0; i < z.length - 1; i++) {
-            for (int j = 0; j < z.length - i - 1; j++) {
-                if (z[j] > z[j + 1]) {
-                    int k = z[j];
-                    z[j] = z[j + 1];
-                    z[j + 1] = k;
+        for (int d = 8; d < 24 * 3; d++) {
+            //int size = (1 << d)/12;
+            int size = d * 1000;
+
+            int z[] = new int[size];
+            Random rnd = new Random();
+            for (int i = 0; i < z.length; i++) {
+                z[i] = rnd.nextInt(255);
+            }
+            int y[] = z.clone();
+
+//С прерыванием по наличию пузырька.
+            long startTime = System.currentTimeMillis();
+            for (int i = 0; i < z.length - 1; i++) {
+                hasBubble = false;
+                for (int j = 0; j < z.length - i - 1; j++) {
+                    if (z[j] > z[j + 1]) {
+                        hasBubble = true;
+                        k = z[j];
+                        z[j] = z[j + 1];
+                        z[j + 1] = k;
+                    }
+                }
+                if (!hasBubble) break;
+            }
+            System.out.println("Размер " + size + ". С прерыванием по наличию пузырька: " + (System.currentTimeMillis() - startTime) + " ms");
+
+//Без прерывания по наличию пузырька.
+            startTime = System.currentTimeMillis();
+            for (int i = 0; i < y.length - 1; i++) {
+                for (int j = 0; j < y.length - i - 1; j++) {
+                    if (y[j] > y[j + 1]) {
+                        k = y[j];
+                        y[j] = y[j + 1];
+                        y[j + 1] = k;
+                    }
                 }
             }
+            System.out.println("Размер " + size + ". Без прерывания по наличию пузырька: " + (System.currentTimeMillis() - startTime) + " ms\n");
+
+            //System.out.println("Равны ли: " + Arrays.equals(z, y));
         }
-        System.out.println(Arrays.toString(z));
+
     }
 }
