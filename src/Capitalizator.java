e7d62c643dcac23cbae398dae493ad43c2e8e76a
r3bers <mike@solovjov.net>
2016-02-26 01:11:51 +0300

    Одно задание и правки.

--- src/Capitalizator.java
+++ src/Capitalizator.java
@@ -1,3 +1,5 @@
+import java.util.StringTokenizer;
+
 /**
  * Created by r3bers on 25.02.2016.
  * Задания от Geekbrains
@@ -6,7 +8,20 @@
  */
 public class Capitalizator {
     public static void main(String[] args) {
-        String str = "ЭТО настояшая пРоверка Букв";
+        String str = "ЭТО настояшая пРоверка Букв!";
+        System.out.println(str);
+
+        StringTokenizer st = new StringTokenizer(str, " \t\n\r,.!?");
+        while (st.hasMoreTokens()) {
+            String slovo = st.nextToken();
+            char arr[] = slovo.toCharArray();
+            arr[0] = Character.toUpperCase(arr[0]);
+            for (int i = 1; i < arr.length; i++) {
+                arr[i] = Character.toLowerCase(arr[i]);
+            }
+            String rightWord = String.valueOf(arr);
+            str = str.replaceAll(slovo, rightWord);
+        }
         System.out.println(str);
     }
 }
