c6548c4e8f88cdca909434cafef3e23535dfe223
r3bers <mike@solovjov.net>
2016-02-25 06:03:30 +0300

    Пару заданий вокруг Binary Search

---
+++ src/Capitalizator.java
@@ -0,0 +1,12 @@
+/**
+ * Created by r3bers on 25.02.2016.
+ * Задания от Geekbrains
+ * Написать программу, которая принимает строку и делает все слова заглавными:
+ * “ЭТО настояшая пРоверка Букв” => “Это Настоящая Проверка Букв”
+ */
+public class Capitalizator {
+    public static void main(String[] args) {
+        String str = "ЭТО настояшая пРоверка Букв";
+        System.out.println(str);
+    }
+}
